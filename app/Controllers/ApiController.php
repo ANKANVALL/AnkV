67ab5fecc40f449bbab7833b13e54156b3dae3be
ANKANVALL <126203081+ANKANVALL@users.noreply.github.com>
2025-07-16 10:14:51 -0600

    Update ApiController.php

--- app/Controllers/ApiController.php
+++ app/Controllers/ApiController.php
@@ -7,7 +7,7 @@ class ApiController{
 
     public function index_rivals(){
      $api =   new Api_Manager('https://marvelrivalsapi.com/api/v1',[
-            'x-api-key: 91b4b2320eb03e8acdf3cdf9081a366b72a41b1081a30540a438dfd76190c4d0'
+            'x-api-key: '
         ]);
         $response = $api->get('/heroes');
 
