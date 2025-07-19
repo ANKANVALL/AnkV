fef52996830d9638faf20203abd4926c26f82bda
ANKANVALL <luis.fregoso.reol.html@gmail.com>
2025-07-19 09:02:22 -0600

    Pruebas de commit remoto

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,7 +4,7 @@ require_once __DIR__.'/../database/DbClass.php';
 class UserModel extends Db{
  
     public function Read(){
-        $this->query('SELECT * FROM users');
+        $this->query('SELECT * FROM usuario');
         return $this->resultset();
     }
 
